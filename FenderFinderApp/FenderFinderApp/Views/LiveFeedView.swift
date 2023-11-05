d1a343bae529716257719f3dc3770c5281f49489
Mukund Raman <mkraman@Mukunds-MacBook-Air.local>
2023-11-05 06:01:24 -0600

    minor changes again

--- FenderFinderApp/FenderFinderApp/Views/LiveFeedView.swift
+++ FenderFinderApp/FenderFinderApp/Views/LiveFeedView.swift
@@ -49,16 +49,35 @@ class LiveFeedViewModel: ObservableObject {
     
     private var cancellables: Set<AnyCancellable> = []
     private var interpreter: Interpreter
+    private var labels: [String] = []
+    
+//    init() {
+//        guard let modelPath = Bundle.main.path(forResource: "default", ofType: "tflite") else {
+//            fatalError("Failed to find the model file.")
+//        }
+//        do {
+//            interpreter = try Interpreter(modelPath: modelPath)
+//            try interpreter.allocateTensors()
+//        } catch {
+//            fatalError("Failed to create the interpreter with error: \(error.localizedDescription)")
+//        }
+//    }
     
     init() {
-        guard let modelPath = Bundle.main.path(forResource: "default", ofType: "tflite") else {
-            fatalError("Failed to find the model file.")
+        guard let modelPath = Bundle.main.path(forResource: "default", ofType: "tflite"),
+              let labelsPath = Bundle.main.path(forResource: "labels", ofType: "txt") else {
+            fatalError("Failed to find the model file or labels.")
         }
         do {
+            // Load the labels
+            let labelsContent = try String(contentsOfFile: labelsPath)
+            labels = labelsContent.components(separatedBy: .newlines)
+            
+            // Initialize the interpreter
             interpreter = try Interpreter(modelPath: modelPath)
             try interpreter.allocateTensors()
         } catch {
-            fatalError("Failed to create the interpreter with error: \(error.localizedDescription)")
+            fatalError("Failed to load the model or labels with error: \(error.localizedDescription)")
         }
     }
     
@@ -80,20 +99,44 @@ class LiveFeedViewModel: ObservableObject {
     }
     
 //    private func performInference(on pixelBuffer: CVPixelBuffer) -> Bool {
-//        // Preprocess the frame to the format the TensorFlow Lite model expects
+//        // Ensure that the pixel buffer is in the correct format (RGB, etc.)
+//        guard let rgbData = preprocess(pixelBuffer: pixelBuffer, modelInputSize: CGSize(width: 168, height: 224)) else {
+//            print("Failed to preprocess pixel buffer")
+//            return false
+//        }
+//
 //        // Perform inference with the TensorFlow Lite model
-//        // Interpret the results to determine if a crash is detected
-//        // This is a simplified placeholder logic
 //        do {
-//            try interpreter.copy(Data(), toInputAt: 0)
+//            // Copy the RGB data to the input tensor
+//            try interpreter.copy(rgbData, toInputAt: 0)
+//            
+//            // Run inference by invoking the interpreter
 //            try interpreter.invoke()
+//            
+//            // Get the output tensor data from the interpreter
 //            let outputTensor = try interpreter.output(at: 0)
+//            
 //            // Interpret the output tensor data to detect a crash
+//            // The logic here will depend on the output format of your model
+//            // For example, if your model outputs a single float value as a probability:
+//            let probabilities = outputTensor.data.toArray(type: Float.self)
+//            let crashProbability = probabilities.first ?? 0
+//            
+//            // Determine if a crash is detected based on the model's probability
+//            // This threshold can be adjusted based on your model's behavior
+//            let crashThreshold: Float = 0.5
+//            let isCrashDetected = crashProbability > crashThreshold
+//            
+//            if isCrashDetected {
+//                print("CRASH DETECTED!!!")
+//            }
+//            
 //            // Return true if a crash is detected, false otherwise
+//            return isCrashDetected
 //        } catch {
 //            print("Failed to perform inference: \(error)")
+//            return false
 //        }
-//        return false
 //    }
     
     private func performInference(on pixelBuffer: CVPixelBuffer) -> Bool {
@@ -115,18 +158,23 @@ class LiveFeedViewModel: ObservableObject {
             let outputTensor = try interpreter.output(at: 0)
             
             // Interpret the output tensor data to detect a crash
-            // The logic here will depend on the output format of your model
-            // For example, if your model outputs a single float value as a probability:
-            let probabilities = outputTensor.data.toArray(type: Float.self)
-            let crashProbability = probabilities.first ?? 0
+            let outputData = outputTensor.data.toArray(type: Float.self)
+            print(outputData)
             
-            // Determine if a crash is detected based on the model's probability
-            // This threshold can be adjusted based on your model's behavior
-            let crashThreshold: Float = 0.5
-            let isCrashDetected = crashProbability > crashThreshold
+            // Find the index of the highest confidence value
+            guard let maxConfidenceIndex = outputData.indices.max(by: { outputData[$0] < outputData[$1] }) else {
+                print("Failed to find the index with the highest confidence.")
+                return false
+            }
+            
+            // Get the label with the highest confidence
+            let detectedLabel = labels[maxConfidenceIndex]
+            
+            // Determine if a crash is detected based on the label
+            let isCrashDetected = detectedLabel.lowercased().contains("crash")
             
             if isCrashDetected {
-                print("CRASH DETECTED!!!")
+                print("CRASH DETECTED!!! Label: \(detectedLabel)")
             }
             
             // Return true if a crash is detected, false otherwise
