8184ad995bdb249424b470699ff0f8b82471a2e6
Adarsh Goura <adarshg1219@gmail.com>
2023-11-04 22:02:37 -0500

    First Push

---
+++ FenderFinderApp/FenderFinderApp/FenderFinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FenderFinderAppApp.swift
+//  FenderFinderApp
+//
+//  Created by Adarsh Goura on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FenderFinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
